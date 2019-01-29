b2ef18d6d6c5aab408b589213a086e7e5698ed94
David Vlasák <31956784+dejvones@users.noreply.github.com>
2019-01-29 11:17:24 +0100

    Už to jede
    Ale, problíkává to a statistika se aktualizuje až po entru

--- pocitani-zs.py
+++ pocitani-zs.py
@@ -8,6 +8,7 @@ Created on Tue Jan 22 10:00:38 2019
 
 import tkinter as tk
 from tkinter import Entry, LabelFrame, Checkbutton, Label, Button
+import random
 
 class Application (tk.Tk):
     name = 'Počítaní'
@@ -21,6 +22,8 @@ class Application (tk.Tk):
         self.OperaceFrame.pack (anchor = tk.W)
         self.RovniceFrame = LabelFrame (self)
         self.RovniceFrame.pack (anchor = tk.W)
+        self.HodnoceniFrame = LabelFrame (self)
+        self.HodnoceniFrame. pack (anchor = tk.W)
         
     #### Operace ####
         self.ScitaniVar = tk.BooleanVar()
@@ -39,27 +42,27 @@ class Application (tk.Tk):
         Checkbutton (self.OperaceFrame, text = '*   ', variable = self.NasobeniVar).grid(row = 3, column = 0)
         Checkbutton (self.OperaceFrame, text = '/   ', variable = self.DeleniVar).grid(row = 4, column = 0)
         
-        Button (self.OperaceFrame, text = 'Spustit', command = self.Priklady).grid (row = 5, column = 2)
+        Button (self.OperaceFrame, text = 'Spustit', command = self.Start).grid (row = 5, column = 3)
     
     #### Rovnice ####
-        self.x = tk.StringVar()
-        self.x.set(0)
+        self.a = tk.StringVar()
+        self.a.set(0)
         self.operant = tk.StringVar()
         self.operant.set (' + ')
-        self.y = tk.StringVar()
-        self.y.set(0)
+        self.b = tk.StringVar()
+        self.b.set(0)
         self.vysledek = tk.StringVar()
         self.vysledek.set(0)
         
         Label (self.RovniceFrame, text = 'Rovnice:').grid(row = 6, column = 0)
         
-        self.AEntry = Entry (self.RovniceFrame, state = 'readonly', textvariable = self.x, width = 4)
+        self.AEntry = Entry (self.RovniceFrame, state = 'readonly', textvariable = self.a, width = 4)
         self.AEntry.grid (row = 7, column = 0)
         
         self.OperantLab = Label (self.RovniceFrame, textvariable = self.operant)
         self.OperantLab.grid (row = 7, column = 1)
         
-        self.BEntry = Entry (self.RovniceFrame, state = 'readonly', textvariable = self.y, width = 4)
+        self.BEntry = Entry (self.RovniceFrame, state = 'readonly', textvariable = self.b, width = 4)
         self.BEntry.grid (row = 7, column = 2)
         
         self.RovnaseLab = Label (self.RovniceFrame, text = ' = ')
@@ -67,9 +70,73 @@ class Application (tk.Tk):
         
         self.vysledekEntry = Entry (self.RovniceFrame, textvariable = self.vysledek, width = 4)
         self.vysledekEntry.grid (row = 7, column = 4)
+        self.bind ('<Return>', self.kontrola)
+    
+    #### Hodnoceni ####
+        Label (self.HodnoceniFrame, text = 'Hodnoceni').grid(row = 8, column = 0)
+        self.OKLabel = Label (self.HodnoceniFrame, text = 'Počítej')
+        self.OKLabel.grid (row = 9, column = 0)
+        self.StatLabel = Label (self.HodnoceniFrame, text = '0 / 0')
+        self.StatLabel.grid (row = 9, column = 3)
+      
+    def Start (self):
+        self.dobre = 0
+        self.celkem = 0
+        self.Priklady()
+    def Priklady (self):
+        self.vyber = []
+        if  self.ScitaniVar.get() == 1:
+            self.vyber.append (' + ')
+        if self.OdcitaniVar.get() == 1:
+            self.vyber.append (' - ')
+        if self.NasobeniVar.get ()== 1:
+            self.vyber.append (' * ')
+        if self.DeleniVar.get ()== 1:
+            self.vyber.append (' / ')
+        print (self.vyber)   
+        operace = random.choice(self.vyber)
+        print(operace)
         
-        def Priklady (self):
-            pass
+        if operace == ' + ':
+            self.CisloA = random.randint (1,50)
+            self.CisloB = random.randint (1,50)
+            self.Vysledek = self.CisloA + self.CisloB
+        
+        if operace == ' - ':
+            self.CisloA = random.randint (1,50)
+            self.CisloB = random.randint (1,self.CisloA)
+            self.Vysledek = self.CisloA - self.CisloB
+           
+        if operace == ' * ':
+            self.CisloA = random.randint (1,10)
+            self.CisloB = random.randint (1,10)
+            self.Vysledek = self.CisloA * self.CisloB
+        
+        if operace == ' / ':
+            self.CisloA = random.randint (1,100)
+            self.CisloB = random.randint (1,10)
+            self.Vysledek = self.CisloA / self.CisloB
+        
+        self.a.set(self.CisloA)
+        self.b.set(self.CisloB)
+        self.operant.set (operace)
+    
+    def kontrola(self, x):
+        try:
+            if self.Vysledek == int (self.vysledekEntry.get()):
+                self.OKLabel.config (text = 'OK')
+                self.dobre += 1
+            else:
+                self.OKLabel.config (text = 'Spíš ne')
+            self.celkem += 1
+            self.vysledek.set(0)
+            self.StatLabel.config (text = '{0} / {1}'.format(self.dobre, self.celkem))
+            self.Priklady()
+        except:
+            self.OKLabel.config (text = 'Počítej')
+        
+        
+            
 
 
 app = Application()
